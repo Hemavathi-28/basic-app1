04db813888b93e7d3a69ac673047cbb2cd2fde49
Hemavathi-28 <hemavathiselvaraj20@gmail.com>
2021-04-01 19:35:04 +0530

    first commit

---
+++ todo.py
@@ -0,0 +1,39 @@
+import PySimpleGUI as sg
+tasks=[]
+layout = [
+    [sg.InputText(size = (40,1), font=("Arial",16), key='add_item'),
+     sg.Button("Add",font=("Arial",16), key='add_save')],
+    [sg.Listbox(values=tasks, size=(40,10),font=("Arial",16),key='items'),
+     sg.Button("Edit",font=("Arial",18),key='edit'),
+     sg.Button("Delete",font=("Arial",18),key='delete')]
+]
+def add_item():
+    item=values['add_item']  #it will indicate the input which we give as item
+    tasks.append(item)
+    window.FindElement('items').Update(values=tasks)
+    window.FindElement('add_item').Update("")
+    window.FindElement('add_save').Update('Add')
+
+def delete_item():
+    delete_val=values['items'][0]
+    tasks.remove(delete_val)
+    window.FindElement('items').Update(values=tasks)
+
+def edit_item():  #when click edit that item remove from list box added to text box also add button changes to save button
+    edit_val=values['items'][0]
+    tasks.remove(edit_val)
+    window.FindElement('items').Update(values=tasks)
+    window.FindElement('add_item').Update(value=edit_val)
+    window.FindElement('add_save').Update('Save')
+
+window=sg.Window("ToDoList",layout)
+while True:
+    event,values=window.Read()
+    if event == sg.WINDOW_CLOSED:
+        break
+    elif event == 'add_save':
+        add_item()
+    elif event == 'delete':
+        delete_item()
+    elif event == 'edit':
+        edit_item()
